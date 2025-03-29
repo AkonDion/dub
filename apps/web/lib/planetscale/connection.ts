02ead601c531627d2b1a5a8883fb03b3049d97b8
AkonDion <166956966+AkonDion@users.noreply.github.com>
2025-03-28 23:06:37 -0400

    fix

--- apps/web/lib/planetscale/connection.ts
+++ apps/web/lib/planetscale/connection.ts
@@ -2,7 +2,18 @@ import { PrismaClient } from "@prisma/client";
 import { PrismaPlanetScale } from "@prisma/adapter-planetscale";
 import { Client } from "@planetscale/database";
 
-const client = new Client({ url: process.env.DATABASE_URL });
+const client = new Client({
+  url: process.env.DATABASE_URL,
+  fetch: (url, init) => {
+    return fetch(url, {
+      ...init,
+      cache: "no-store",
+    });
+  },
+  ssl: {
+    rejectUnauthorized: true
+  }
+});
 const adapter = new PrismaPlanetScale(client);
 
 export const conn = new PrismaClient({ adapter });
